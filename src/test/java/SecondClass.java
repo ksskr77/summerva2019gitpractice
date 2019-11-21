8c33f568aaa8548b9a4fcd4b78913e50d72ccaae
Sunsoo Kim <ksskr77@gmail.com>
2019-11-21 16:46:27 -0500

    this is another commit

--- src/test/java/SecondClass.java
+++ src/test/java/SecondClass.java
@@ -1,7 +1,8 @@
 public class SecondClass {
     public static void main(String[] args) {
-        String name = "Yerim";
+        String name = "Yoonjin";
         System.out.println(name);
+        System.out.println("Yerim");
         
     }
 }
