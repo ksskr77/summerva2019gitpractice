a11a8f78e6b665fe9f364499d03a9615c172a85d
Sunsoo Kim <ksskr77@gmail.com>
2019-11-21 16:32:01 -0500

    second commit

--- src/test/java/FirstClass.java
+++ src/test/java/FirstClass.java
@@ -2,5 +2,9 @@ public class FirstClass {
 
     public static void main(String[] args) {
         System.out.println("Hello world");
+        System.out.println("I am working");
+        System.out.println("Hello world");
+
+
     }
 }
---
+++ src/test/java/SecondClass.java
@@ -0,0 +1,7 @@
+public class SecondClass {
+    public static void main(String[] args) {
+        String name = "Yoonjin";
+        System.out.println(name);
+
+    }
+}
