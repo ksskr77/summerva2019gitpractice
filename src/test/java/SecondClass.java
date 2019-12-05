e8df0a9623fbb9986054bbfde1a403e5a9a34764
Sunsoo Kim <ksskr77@gmail.com>
2019-12-05 16:30:54 -0500

    fixed conflict yoonjin

--- src/test/java/SecondClass.java
+++ src/test/java/SecondClass.java
@@ -3,6 +3,7 @@ public class SecondClass {
         String name = "Yoonjin";
         System.out.println(name);
         System.out.println("Yerim the second");
+        System.out.println("Only YIM");
                 System.out.println("I am working until 5");
         System.out.println("I am working until 5");
 
