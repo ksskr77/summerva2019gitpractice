65765c3c2a8ab92154e8597a3850e2cc9eedb311
ksskr77 <55106417+ksskr77@users.noreply.github.com>
2019-12-05 15:45:24 -0500

    Update SecondClass.java

--- src/test/java/SecondClass.java
+++ src/test/java/SecondClass.java
@@ -2,7 +2,7 @@ public class SecondClass {
     public static void main(String[] args) {
         String name = "Yoonjin";
         System.out.println(name);
-        System.out.println("Yerim");
+        System.out.println("Yerim second");
                 System.out.println("I am working until 5");
         System.out.println("I am working until 5");
 
