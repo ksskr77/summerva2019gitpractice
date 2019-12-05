e889d2939fe46267bd5e28f1b2dfd780335db0f2
ksskr77 <55106417+ksskr77@users.noreply.github.com>
2019-12-05 15:28:49 -0500

    Update FirstClass.java

--- src/test/java/FirstClass.java
+++ src/test/java/FirstClass.java
@@ -1,12 +1,10 @@
 public class FirstClass {
 
     public static void main(String[] args) {
-        System.out.println("Hi, world");
+        System.out.println("hello, yoonjin");
         System.out.println("I am working until 5");
         System.out.println("Hello world");
-        Yoonjin
-            Yoonjin
-            Yoonjin
+
             
 
 
