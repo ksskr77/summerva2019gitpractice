cdb023f2dc682460dd1fac44c9af550ac3121a71
ksskr77 <55106417+ksskr77@users.noreply.github.com>
2019-12-05 14:37:03 -0500

    Update FirstClass.java

--- src/test/java/FirstClass.java
+++ src/test/java/FirstClass.java
@@ -1,7 +1,7 @@
 public class FirstClass {
 
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("Hi world");
         System.out.println("I am working until 5");
         System.out.println("Hello world");
 
