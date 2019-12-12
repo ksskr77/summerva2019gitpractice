09d0ba30291fd602d0b17320b09c9774f30f4a80
Sunsoo Kim <ksskr77@gmail.com>
2019-12-12 10:43:35 -0500

    changed develop branch

--- src/test/java/SecondClass.java
+++ src/test/java/SecondClass.java
@@ -2,10 +2,10 @@ public class SecondClass {
     public static void main(String[] args) {
         String name = "Yoonjin";
         System.out.println(name);
+        System.out.println(name);
+        System.out.println(name);
         System.out.println("Yerim the second");
         System.out.println("Only YIM");
-                System.out.println("I am working until 5");
-        System.out.println("I am working until 5");
 
         
     }
