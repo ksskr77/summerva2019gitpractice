0f921641c4dc37e3ce33edc64a49e5898511a0b4
Sunsoo Kim <ksskr77@gmail.com>
2019-12-12 11:50:28 -0500

    compare

--- src/test/java/SecondClass.java
+++ src/test/java/SecondClass.java
@@ -1,12 +1,8 @@
 public class SecondClass {
     public static void main(String[] args) {
         String name = "Yoonjin";
-        System.out.println(name);
-        System.out.println(name);
-        System.out.println(name);
         System.out.println("miss Yoonjin");
-        System.out.println("Yerim the second");
-        System.out.println("Only YIM");
+        System.out.println("Only Yoonjin");
 
         
     }
